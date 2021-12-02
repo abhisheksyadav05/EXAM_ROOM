0dbbee6694f35941a6f1f3ae4915f66595813fe2
kinjalvarshney <68551027+kinjalvarshney@users.noreply.github.com>
2021-12-02 23:18:53 +0530

    Delete RoleRepository.java

--- RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.examroom.backend.repo;
-
-import com.examroom.backend.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-
-}
