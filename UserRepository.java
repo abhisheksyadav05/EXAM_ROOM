9f290be5bd6f70adaecfc9765347eebecf0159ee
kinjalvarshney <68551027+kinjalvarshney@users.noreply.github.com>
2021-12-02 23:19:13 +0530

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.examroom.backend.repo;
-
-import com.examroom.backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-    public User findByUsername(String username);
-}
