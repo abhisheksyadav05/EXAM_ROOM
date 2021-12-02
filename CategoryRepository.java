97ca4139c08beafdf85574f50a1f350bda95938f
kinjalvarshney <68551027+kinjalvarshney@users.noreply.github.com>
2021-12-02 23:19:29 +0530

    Delete CategoryRepository.java

--- CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.examroom.backend.repo;
-
-import com.examroom.backend.model.exam.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository  extends JpaRepository<Category,Long>{
-
-}
